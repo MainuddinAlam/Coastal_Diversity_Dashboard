355fdc743d96753b97756f133c660189b61b7080
Mainuddin Alam Irteja <mainuddinalamirteja@gmail.com>
2024-11-13 12:47:49 -0400

    Created .streamlit folder

--- App.py
+++ App.py
@@ -1,4 +1,15 @@
-import streamlit as st
+"""
+Code author:- Mainuddin Alam Irteja
 
-st.title('Hello World Streamlit App')
+Source of data:-
+"""
+
+# Import important modules needed for the application
+import streamlit as st
+import pandas as pd
+import plotly as plt
+
+# Assign the tab name and title of the page respectively
+st.set_page_config(page_title="Coastal Research Dashboard")
+st.title('Coastal Research Dashboard: Water Quality and Biodiversity Analysis')
 st.write('Welcome to the world of Streamlit!')
