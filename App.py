a0c373db358c167b08578c15e0c2b4ae927e2a42
Mainuddin Alam Irteja <mainuddinalamirteja@gmail.com>
2024-11-14 19:02:26 -0400

    Working on sidebar code

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 """
 Code author:- Mainuddin Alam Irteja
 
-Source of data:-
+Source of data:- w
 """
 
 # Import important modules needed for the application
@@ -9,7 +9,30 @@ import streamlit as st
 import pandas as pd
 import plotly as plt
 
-# Assign the tab name and title of the page respectively
-st.set_page_config(page_title="Coastal Research Dashboard")
-st.title('Coastal Research Dashboard: Water Quality and Biodiversity Analysis')
-st.write('Welcome to the world of Streamlit!')
+# Function to set the title and tab name
+def set_Initial():
+    st.set_page_config(page_title="Coastal Diversity Research Dashboard")
+    st.title("Coastal Diversity Research Dashboard")
+    st.write("")
+
+ 
+
+# Function to read the given data file
+def read_csv_file():
+    csv_path = "./datas/SeineCatch.csv"
+    seine_catch_csv = pd.read_csv(csv_path)
+    return seine_catch_csv
+ 
+# Set the title and tab name
+set_Initial()
+
+# Get the data
+get_data = read_csv_file()
+
+
+
+# Sidebar menu with borderless buttons
+st.sidebar.title("Table of Contents")
+home_button = st.sidebar.button("1. Home")
+about_button = st.sidebar.button("2. Overview")
+contact_button = st.sidebar.button("3. Analysis")
